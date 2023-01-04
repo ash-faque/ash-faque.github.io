b5f3bddae791a9bdf8c1262fed9869f2a3a77f2c
ash-faque <ashfaqueppbtr@gmail.com>
2023-01-04 07:01:07 +0530

    neo brutalism or something like that

--- script.js
+++ script.js
@@ -11,15 +11,3 @@ const appendSocials = (socials, socials_div) => {
     });
 }
 appendSocials(socials, socials_div)
-
-
-const body = document.querySelector('body');
-const alterBg = (elm, intervl = 1) => {
-    let hue = 0
-    setInterval(() => {
-        let random_color = `hsl(${hue}, 20%, 80%)`
-        elm.style.backgroundColor = random_color
-        hue > 359 ? hue = 0 : hue += 6
-    }, intervl * 1000)
-}
-alterBg(body, 1)
