9bbb72e012dcce5ffa26bad8ac121c2e1a19b81c
ash-faque <ashfaqueppbtr@gmail.com>
2023-02-16 17:33:08 +0530

    ede

--- rtc/h/script.js
+++
@@ -1,9 +0,0 @@
-const main_canvas = document.getElementById('main_canvas')
-const ctx = main_canvas.getContext('2d')
-
-// console.log(ctx)
-
-import PieSocket from "./network.js"
-const socket = new PieSocket()
-
-socket.sendMessage('jilaabi')
--- xkcd.js
+++ xkcd.js
@@ -1,8 +1,6 @@
 export default async (target, comic = 'latest') => {
 
     let json_response = await fetch(`https://xkcd.vercel.app/?comic=${comic}`)
-    
-    // console.log(json_response)
 
     let {
         month,
@@ -18,26 +16,11 @@ export default async (target, comic = 'latest') => {
         day
     } = await json_response.json()
 
-    // console.log(
-    //     month,
-    //     num,
-    //     link,
-    //     year,
-    //     news,
-    //     safe_title,
-    //     transcript,
-    //     alt,
-    //     img,
-    //     title,
-    //     day
-    // )
-
     target.innerHTML += `<div class="comic">
         <img src="${img}" alt="${alt}">
         <div class="comic_detail">
             <h4>${safe_title ? safe_title : title }</h4>
             <p><b>${num}</b> - <em>${day}:${month}:${year}</em></p>
-            <p>${transcript}</p>
             <p>${news}</p>
             ${(link.length) ? `<a href="${link}" target="_blank">@xkcd</a>` : ''}
         </div>
